8b5f3529bf9e6105dd49b10368d5736da786eb97
Hartmut Reuter <reuter@rzg.mpg.de>
2011-03-28 13:27:38 +0000

    Correct debugging output

--- src/rxosd/rxosd.c
+++ src/rxosd/rxosd.c
@@ -2540,7 +2540,7 @@ incdec(struct rx_call *call, struct oparmT10 *o, afs_int32 diff)
     char string[FIDSTRLEN];
 
     extract_oparmT10(o, &lun, &vid, &vnode, &unique, NULL);
-    ViceLog(1,("SRXOSD_incdec for %s from %u.%u.%u.%u\n",
+    ViceLog(1,("SRXOSD_incdec for %s %d from %u.%u.%u.%u\n",
                 sprint_oparmT10(o, string, sizeof(string)),
                 diff,
                 (ntohl(call->conn->peer->host) >> 24) & 0xff,
