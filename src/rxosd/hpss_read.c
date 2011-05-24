5a80bdcee08077b883639e26d72315056a35e89e
Hartmut Reuter <reuter@rzg.mpg.de>
2011-05-24 06:45:44 +0000

    Try to authenticate twice as thay do in the hpss examples

--- src/rxosd/hpss_read.c
+++ src/rxosd/hpss_read.c
@@ -79,7 +79,7 @@ char **argv;
 				"/usr/afs/etc/afsipp.keytab");
     if (code) {
 	fprintf(stderr, "hpss_SetLoginCred failed with %d retrying in 5 seconds\n", code);
-	sleep(5)
+	sleep(5);
         code = hpss_SetLoginCred("afsipp", hpss_authn_mech_krb5,
 				hpss_rpc_cred_client,
 				hpss_rpc_auth_type_keytab,
