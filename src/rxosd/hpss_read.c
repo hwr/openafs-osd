abc33d54f43c173bfed8c74280dddf3f833a7231
Hartmut Reuter <reuter@rzg.mpg.de>
2011-05-24 11:25:31 +0000

    Compute Length correctly

--- src/rxosd/hpss_read.c
+++ src/rxosd/hpss_read.c
@@ -154,7 +154,7 @@ char **argv;
     page = 0;
     if (argc < 3) {
 #ifdef AFS_AIX53_ENV
-	fileLength = (status.st_size.high << 32) + status.st_size.low;
+	fileLength = ((afs_uint64)status.st_size.high << 32) + status.st_size.low;
 #else
         fileLength = status.st_size;
 #endif
