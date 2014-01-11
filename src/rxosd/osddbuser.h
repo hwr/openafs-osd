918a95aab091e18fb975a824ed5b912f068d819d
Achim Gsell <achim.gsell@psi.ch>
2014-01-11 21:44:35 +0100

    src/rxosd/osddbuser.h: - fillRxEndpoint(): ptr-arg 'type' changed to unsigned
    - declarations added

--- src/rxosd/osddbuser.h
+++ src/rxosd/osddbuser.h
@@ -18,7 +18,7 @@ extern struct ubik_client *osddb_client;
 extern void FillPolicyTable(void);
 extern void FillOsdTable(void);
 extern afs_uint32 MinOsdWipeMB(afs_uint32 osd);
-extern afs_int32 fillRxEndpoint(afs_uint32 id, struct rx_endp *endp, afs_int32 *type,
+extern afs_int32 fillRxEndpoint(afs_uint32 id, struct rx_endp *endp, afs_uint32 *type,
 			        afs_int32 ignore);
 extern afs_int32 FindOsdType(afs_uint32 id, afs_uint32 *ip, afs_uint32 *lun,
 			     afs_int32 ignore, afs_uint32 *type, afs_uint32 *service,
@@ -41,3 +41,11 @@ extern afs_int32 get_restore_cand(afs_uint32 nosds, afs_uint32 *osd);
 extern afs_int32 OsdHasAccessToHSM(afs_uint32 osd_id);
 extern afs_int32 policy_uses_file_name(afs_int32 policyIndex);
 extern struct ubik_client * init_osddb_client(char *cell);
+extern afs_uint64 get_max_move_osd_size(void);
+extern afs_int32 eval_policy(unsigned int policyIndex, afs_uint64 size, char *fileName,
+                             afs_int32 (*evalclient) (void *rock, afs_int32 user),
+                             void *client, afs_uint32 *use_osd, afs_uint32 *dyn_location,
+                             afs_uint32 *stripes, afs_uint32 *stripe_size, afs_uint32 *copies,
+                             afs_uint32 *force);
+
+extern void display_policy_by_id(afs_uint32 id, int format, int unroll, struct ubik_client *uc);
