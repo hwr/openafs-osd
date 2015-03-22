a41949ae3914047ffd67a89d27f71d5a181d32af
Hartmut Reuter <reuter@rzg.mpg.de>
2015-03-22 17:59:29 +0100

    Use always DumpV2
    It happend that the flags were not transferred to the volserver.

--- src/volser/vsprocs.c
+++ src/volser/vsprocs.c
@@ -4401,10 +4401,7 @@ UV_DumpVolume(afs_uint32 afromvol, afs_uint32 afromserver, afs_int32 afrompart,
     fromcall = rx_NewCall(fromconn);
 
     VEPRINT1("Starting volume dump on volume %u...", afromvol);
-    if (flags & VOLDUMPV2_OMITDIRS)
-	code = StartAFSVolDumpV2(fromcall, fromtid, fromdate, flags);
-    else
-	code = StartAFSVolDump(fromcall, fromtid, fromdate);
+    code = StartAFSVolDumpV2(fromcall, fromtid, fromdate, flags);
     EGOTO(error_exit, code, "Could not start the dump process \n");
     VEDONE;
 
